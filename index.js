4ae4f54d9c4c5a1d718290cf022b18a5ccde3601
Fardos Mohamed <ffm2@uw.edu>
2016-01-19 17:14:18 -0800

    Finished working on index

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+/**
+* @fileOverview Exports a function that finds the previous and the next day in a date format.
+* @name Index
+*/
 'use strict';
-
+ 
+ 
 var table = {
-	after: 1,
-	before: -1
+                after: 1,
+                before: -1
 };
-
-module.exports = function (gap) {
-	gap = table[gap] || 0;
-
-	var date = new Date();
-	date.setDate(date.getDate() + gap);
-
-	return date;
+ 
+/**
+* This method returns the previous, next or same day in a date format.
+* @param {string} [gap] A gap from today. It can be 'before' or 'after'.
+* @returns {object} A date (current, previous, or next)
+*/
+module.exports = function (gap) {  
+    /**
+     * Gap can be -1, +1 if table[gap] is delcared, otherwise it is set to 0.
+     */
+                gap = table[gap] || 0;
+ 
+                var date = new Date();
+                date.setDate(date.getDate() + gap);
+               
+                return date;
 };
